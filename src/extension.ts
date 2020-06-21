03e40fc4e210a789d620c3b73d7c766e7bd79099
Yue Zhang <contact@zhangyue.dev>
2020-06-21 15:27:15 -0400

    Get access modifier.

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { showModuleNameDialog } from './new/showModuleNameDialog';
+import { showDialog } from './new/showDialog';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	vscode.commands.registerCommand('rust-mod-generator.showModuleNameDialog', showModuleNameDialog);
+	vscode.commands.registerCommand('rust-mod-generator.showModuleNameDialog', showDialog);
 }
 
 
--- src/new/showDialog.ts
+++ src/new/showDialog.ts
@@ -3,7 +3,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { createModule } from "./createModule";
 
-export async function showModuleNameDialog(originUri: vscode.Uri) {
+export async function showDialog(originUri: vscode.Uri) {
     let uri = originUri ? originUri : vscode.window.activeTextEditor?.document.uri;
     if (!uri) {
         vscode.window.showErrorMessage("Please focus on a .rs file .. or just right-click on a .rs file and use the context menu!");
@@ -13,7 +13,14 @@ export async function showModuleNameDialog(originUri: vscode.Uri) {
     let rootPath = path.dirname(uri.fsPath);
     let rootUri = vscode.Uri.file(rootPath);
 
-    vscode.window.showInputBox({ placeHolder: "<directory name> or <file name>.rs", prompt: "Enter the mod name. If the input ends with .rs, a single .rs file will be created, else a new subdirectory with mod.rs will be created.", value: "mod_name(.rs)" }).then(async modName => {
+    let modUri = await getModUri(uri, rootUri);
+    let isPub = await vscode.window.showQuickPick(["private(default)", "pub"], { placeHolder: "pub" }).then(modifier => modifier === "pub" ? true : false);
+
+    vscode.window.showInformationMessage("Is pub? " + isPub);
+}
+
+async function getModUri(uri: vscode.Uri, rootUri: vscode.Uri): Promise<vscode.Uri | undefined> {
+    return vscode.window.showInputBox({ placeHolder: "<directory name> or <file name>.rs", prompt: "Enter the mod name. If the input ends with .rs, a single .rs file will be created, else a new subdirectory with mod.rs will be created.", value: "mod_name(.rs)" }).then(async modName => {
         if (!modName || !uri) {
             return;
         }
@@ -26,7 +33,6 @@ export async function showModuleNameDialog(originUri: vscode.Uri) {
                 fs.existsSync(vscode.Uri.joinPath(rootUri, path.basename(modName, ".rs")).fsPath)) {
                 vscode.window.showErrorMessage("The mod " + path.basename(modName, ".rs") + " already exists.");
                 throw new Error("The mod " + path.basename(modName, ".rs") + " already exists.");
-
             }
 
             await createModule(modUri, false);
@@ -37,10 +43,10 @@ export async function showModuleNameDialog(originUri: vscode.Uri) {
                 fs.existsSync(modUri.fsPath + ".rs")) {
                 vscode.window.showErrorMessage("The mod " + modName + " already exists.");
                 throw new Error("The mod " + modName + " already exists.");
-
             }
 
             await createModule(modUri, true);
         }
+        return modUri;
     });
 }
